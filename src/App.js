33500ac4a47b5483293f4ec91442ac54b542b7fa
FabianNicolasNAU <fabian-almonacid@hotmail.com>
2022-12-12 20:16:52 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = async () => {
+    try {
+       const request = await fetch('https://dummyjson.com/products');
+       const response = await request.json();       
+       setData(response);
+     } catch (error) {
+      console.error(error)
+     }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Products List</h1>  
+      {data && data.products.map((item) => (
+        <div>
+          <p key={item.title}>{item.title}</p>
+          <p key={item.price}>{item.price}</p>
+          <p key={item.description}>{item.description}</p>
+          <img src={item.thumbnail} alt="imagen de producto"/>
+        </div>
+
+      ))}
     </div>
   );
 }
-
 export default App;
